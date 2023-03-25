c1b9fad434bfc73472a676cde650a1fa95e481cb
ValeevAR <Anv-v@yandex.ru>
2023-03-25 19:03:40 +0500

    aaa_async_homework Anvar Valeev

--- abstract_watcher.py
+++ abstract_watcher.py
@@ -1,5 +1,6 @@
 import abc
 from typing import Coroutine, Any
+import asyncio
 
 """
 Описание задачи:
@@ -25,15 +26,19 @@ class AbstractRegistrator(abc.ABC):
     В тестах мы передадим в ваш Watcher нашу реализацию Registrator и проверим корректность сохранения результатов.
     """
 
+    def __init__(self):
+        self.values = []
+        self.errors = []
+
     @abc.abstractmethod
     def register_value(self, value: Any) -> None:
         # Store values returned from done task
-        ...
+        self.values.append(value)
 
     @abc.abstractmethod
     def register_error(self, error: BaseException) -> None:
         # Store exceptions returned from done task
-        ...
+        self.errors.append(error)
 
 
 class AbstractWatcher(abc.ABC):
@@ -42,39 +47,52 @@ class AbstractWatcher(abc.ABC):
     При тестировании мы расчитываем на то, что этот интерфейс будет соблюден.
     """
 
+    @abc.abstractmethod
     def __init__(self, registrator: AbstractRegistrator):
         self.registrator = registrator  # we expect to find registrator here
+        self.tasks = []
 
     @abc.abstractmethod
     async def start(self) -> None:
         # Good idea is to implement here all necessary for start watcher :)
-        ...
+        for task in self.tasks:
+            await task
+            self.registrator.register_value(task.result())
 
     @abc.abstractmethod
     async def stop(self) -> None:
         # Method will be called on the end of the Watcher's work
-        ...
+        pass
 
     @abc.abstractmethod
     def start_and_watch(self, coro: Coroutine) -> None:
-        # Start new task and put to watching
-        ...
+        task = asyncio.create_task(coro)
+        self.tasks.append(task)
+        self.start()
 
 
 class StudentWatcher(AbstractWatcher):
     def __init__(self, registrator: AbstractRegistrator):
         super().__init__(registrator)
         # Your code goes here
-        ...
 
     async def start(self) -> None:
         # Your code goes here
-        ...
+        super().start()
 
     async def stop(self) -> None:
         # Your code goes here
-        ...
+        super().stop()
+
+        for task in self.tasks:
+            try:
+                await task
+                self.registrator.register_value(task.result())
+            except ValueError as ve:
+                self.registrator.register_error(ve)
+            finally:
+                task.cancel()
 
     def start_and_watch(self, coro: Coroutine) -> None:
         # Your code goes here
-        ...
+        super().start_and_watch(coro)
